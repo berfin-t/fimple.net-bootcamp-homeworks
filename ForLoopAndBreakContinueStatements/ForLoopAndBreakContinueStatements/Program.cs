eeb322c21a25640a795c9e593e404ad84c54aded
Berfin Tek <tekberfin@outlook.com>
2023-11-23 20:53:23 +0300

    Add For Loop and Break Continue Statements

---
+++ ForLoopAndBreakContinueStatements/ForLoopAndBreakContinueStatements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i; 
+                else
+                    ciftToplam += i; 
+            }
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Çift Toplam: " + ciftToplam);
+            
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
