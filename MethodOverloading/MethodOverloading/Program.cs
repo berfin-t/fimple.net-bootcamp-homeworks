84ae1835a6d78c9874b268894f378b3611da3edd
Berfin Tek <tekberfin@outlook.com>
2023-11-25 13:19:12 +0300

    Update

--- MethodOverloading/MethodOverloading/Program.cs
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -1,26 +1,47 @@
-﻿//Metot Aşırı Yükleme - Overloading
-int ifade = 999;
-instance.EkranaYazdir(Convert.ToString(ifade));
-instance.EkranaYazdir(ifade);
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-			//Method İmzası
-			//methodAdı + Parametre Sayısı + Parametre Tipi
-		}
-	}
-
-	class Metotlar
+namespace Methods_Overloading
 {
-    public void Topla(int a, int b, out int toplam)
+    public class Program
     {
-        toplam = a + b;
+        static void Main(string[] args)
+        {
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi); if (sonuc)
+            {
+                Console.WriteLine("Başarılı!"); Console.WriteLine(outSayi);
+            }
+            else
+            {
+            }
+            Console.WriteLine("Başarısız!");
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonucu); Console.WriteLine(toplamSonucu);
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+
+        }
     }
-    public void EkranaYazdir(string veri)
+
+    class Metotlar
     {
-        Console.WriteLine(veri);
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
     }
-    public void EkranaYazdir(int veri)
-    {
-        Console.WriteLine(veri);
-    }
-}
 }
