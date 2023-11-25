42eff9079a3c50339b3069d71f39d873dfc827b8
Berfin Tek <tekberfin@outlook.com>
2023-11-25 12:45:24 +0300

    Add MethodOverloading

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿//Metot Aşırı Yükleme - Overloading
+int ifade = 999;
+instance.EkranaYazdir(Convert.ToString(ifade));
+instance.EkranaYazdir(ifade);
+
+			//Method İmzası
+			//methodAdı + Parametre Sayısı + Parametre Tipi
+		}
+	}
+
+	class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a + b;
+    }
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+}
+}
