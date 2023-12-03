335e34bde18ebe7c867c4f4f436ddc441f10e113
Berfin Tek <tekberfin@outlook.com>
2023-12-03 19:14:58 +0300

    Update

---
+++ HomeworkUsingModels/HomeworkUsingModels/Helpers/ErrorHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿namespace WebApi;
+
+using System.Net;
+using System.Text.Json;
+using HttpContent = Microsoft.AspNetCore.Http.HttpContext;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (FluentValidation.ValidationException error)
+        {
+            await HandleExceptionAsync(context, error);
+        }
+        catch (Exception error)
+        {
+            await HandleExceptionAsync(context, error);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContent context, Exception error)
+    {
+        var response = context.Response;
+        var errorObject = new { message = error?.Message };
+        response.ContentType = "application/json";
+
+        switch (error)
+        {
+            case AppException e:
+                // custom application error
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                break;
+            case KeyNotFoundException e:
+                // not found error
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                break;
+            case FluentValidation.ValidationException e:
+                // valdiations errors
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                break;
+            default:
+                // unhandled error
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                break;
+        }
+
+        var result = JsonSerializer.Serialize(errorObject);
+        await response.WriteAsync(result);
+    }
+}
