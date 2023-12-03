d9dcfe11d579543f3ecd0cde70378e026e96f7c7
Berfin Tek <tekberfin@outlook.com>
2023-12-03 18:56:25 +0300

    Update

---
+++ HomeworkUsingModels/HomeworkUsingModels/Entities/Grade.cs
@@ -0,0 +1,14 @@
+﻿using HomeworkUsingModels.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Entities
+{
+    public class Grade
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GradeId { get; set; }
+        public string GradeName { get; set; } = "";
+        public string Section { get; set; } = "";
+        public ICollection<Student>? Students { get; set; }
+    }
+}
